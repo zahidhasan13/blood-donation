222b37914dbbe49fa9a353f45eefad4766654a27
zahidhasan13 <zahidhasanofficial13@gmail.com>
2025-03-07 01:21:23 +0600

    delete and patch route

--- backend/controllers/donarDetailsController.js
+++ backend/controllers/donarDetailsController.js
@@ -1,7 +1,6 @@
 const DonarDetail = require("../models/donarDetailsModel");
 const mongoose = require("mongoose");
 
-
 // Get All Donar Data
 const getAllDonarDetails = async (req, res) => {
   try {
@@ -12,42 +11,78 @@ const getAllDonarDetails = async (req, res) => {
   }
 };
 // Get Single Donar Data
-const getSingleDonarDetails = async (req, res) =>{
-    const {id} = req.params;
+const getSingleDonarDetails = async (req, res) => {
+  const { id } = req.params;
 
-    if(!mongoose.Types.ObjectId.isValid){
-        res.status(404).json({message: "Invalid ID"})
-    }
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(404).json({ message: "Invalid ID" });
+  }
 
-    const donarDetails = await DonarDetail.findById(id);
+  const donarDetails = await DonarDetail.findById(id);
 
-    if(!donarDetails){
-        res.status(404).json({message: "Invalid ID"})
-    }
+  if (!donarDetails) {
+    res.status(404).json({ message: "Invalid ID" });
+  }
 
-    res.status(200).json(donarDetails);
-
-}
+  res.status(200).json(donarDetails);
+};
 // Post Donar Details
 const postDonarDetails = async (req, res) => {
-    const data = req.body;
+  const data = req.body;
 
-    try{
-        const donarDetail = await DonarDetail.create({
-            ...data
-        })
-    
-        res.status(200).json(donarDetail)
-    }catch(err){
-        res.status(400).json({error: err.message})
-    }
-}
+  try {
+    const donarDetail = await DonarDetail.create({
+      ...data,
+    });
 
+    res.status(200).json(donarDetail);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
 
+// Donar Details Delete
+const deleteDonarDetail = async (req, res) => {
+  const { id } = req.params;
 
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(404).json({ message: "Invalid ID" });
+  }
+
+  const donarDetail = await DonarDetail.findOneAndDelete({ _id: id });
+
+  if (!donarDetail) {
+    res.status(404).json({ message: "Invalid ID" });
+  }
+
+  res.status(200).json(donarDetail);
+};
+
+// Donar Details Update
+const updateDonarDetail = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(404).json({ message: "Invalid ID" });
+  }
+
+  const donarDetail = await DonarDetail.findOneAndUpdate(
+    { _id: id },
+    { ...req.body },
+    { new: true }
+  );
+
+  if (!donarDetail) {
+    res.status(404).json({ message: "Invalid ID" });
+  }
+
+  res.status(200).json(donarDetail);
+};
 
 module.exports = {
-    getAllDonarDetails,
-    getSingleDonarDetails,
-    postDonarDetails
-}
+  getAllDonarDetails,
+  getSingleDonarDetails,
+  postDonarDetails,
+  deleteDonarDetail,
+  updateDonarDetail,
+};
--- backend/routes/donarDetailsRoute.js
+++ backend/routes/donarDetailsRoute.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { getAllDonarDetails, getSingleDonarDetails, postDonarDetails } = require("../controllers/donarDetailsController");
+const { getAllDonarDetails, getSingleDonarDetails, postDonarDetails, deleteDonarDetail, updateDonarDetail } = require("../controllers/donarDetailsController");
 
 const router = express.Router();
 
 router.get("/", getAllDonarDetails);
 router.get("/:id", getSingleDonarDetails);
 router.post("/", postDonarDetails)
+router.delete("/:id", deleteDonarDetail);
+router.patch("/:id", updateDonarDetail);
 
 
 
